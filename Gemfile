ad61da3f4d4a3f666f23b97c177d7e15f48659f1
Rowena Koerber <rowenakoerber@gmail.com>
2020-08-12 12:56:15 +0200

    models and seed file

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'simple_form'
 gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
+gem 'faker'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Restaurants are being destroyed'
+Restaurant.destroy_all
+
+random_restaurant = %w[chinese italian japanese french belgian].sample
+
+10.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.city
+  phone = Faker::PhoneNumber.phone_number_with_country_code
+  random_restaurant = %w[chinese italian japanese french belgian].sample
+
+  Restaurant.create(name: name, address: address, phone_number: phone, category: random_restaurant)
+
+  puts "Restaurant #{name} created!"
+end
+
+puts 'All restaurants created'
